ed26dbeebc4dd9c2581116e3cdd2703960f8fdf5
acidify04 <peh4622@naver.com>
2025-08-20 14:33:05 +0900

    initial commit

---
+++ sjseed/sjseed/src/main/java/com/capstone/sjseed/SjseedApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.sjseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjseedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjseedApplication.class, args);
+	}
+
+}
---
+++ sjseed/sjseed/src/test/java/com/capstone/sjseed/SjseedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.sjseed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjseedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
