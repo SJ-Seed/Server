501de87142902f1d65e044a35562347cdba6d88f
acidify04 <peh4622@naver.com>
2025-09-07 14:42:54 +0900

    docs #3: swagger configuration 작성 및 헬스체크 API를 스웨거에 연결

--- sjseed/sjseed/src/main/java/com/capstone/sjseed/controller/health/HealthCheckController.java
+++ sjseed/sjseed/src/main/java/com/capstone/sjseed/controller/health/HealthCheckController.java
@@ -1,5 +1,6 @@
 package com.capstone.sjseed.controller.health;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class HealthCheckController {
 
+    @Operation(summary = "헬스체크 API", description = "서버가 살아있는지 확인합니다.")
     @GetMapping
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("OK");
