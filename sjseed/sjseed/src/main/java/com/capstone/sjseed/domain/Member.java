4b9432098989c361fdbf83cb11a2d2a1129dd04c
acidify04 <peh4622@naver.com>
2025-09-15 22:56:51 +0900

    feat #5: Member entity 생성

---
+++ sjseed/sjseed/src/main/java/com/capstone/sjseed/domain/Member.java
@@ -0,0 +1,44 @@
+package com.capstone.sjseed.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member")
+public class Member extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int year;
+
+    @Column(nullable = false, unique = true)
+    private String loginId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private int coin;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private int attendedDays;
+
+    @Column(nullable = false)
+    @ColumnDefault("false")
+    private boolean premium;
+}
