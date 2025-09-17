406e8434b7866bd5c3ca808694b6325d8fc55b2a
acidify04 <peh4622@naver.com>
2025-09-17 23:04:11 +0900

    feat #7: PlantController에 Swagger 설명 추가

--- sjseed/sjseed/src/main/java/com/capstone/sjseed/controller/PlantController.java
+++ sjseed/sjseed/src/main/java/com/capstone/sjseed/controller/PlantController.java
@@ -3,6 +3,7 @@ package com.capstone.sjseed.controller;
 import com.capstone.sjseed.apiPayload.ApiResponse;
 import com.capstone.sjseed.dto.PlantDetailDto;
 import com.capstone.sjseed.service.PlantService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +14,13 @@ import org.springframework.web.bind.annotation.*;
 public class PlantController {
     private final PlantService plantService;
 
+    @Operation(summary = "식물 상세 정보 조회", description = "자신이 기르고 있는 식물의 상세 정보를 조회합니다.")
     @GetMapping("/detail/{plantId}")
     public ResponseEntity<ApiResponse<PlantDetailDto>> getPlantDetail(@PathVariable Long plantId) {
         return ResponseEntity.ok(ApiResponse.onSuccess(plantService.getPlantDetail(plantId)));
     }
 
+    @Operation(summary = "물 주기 필요 여부 조회", description = "식물에 물을 줘야 하는 주기가 되었는지를 반환합니다.")
     @GetMapping("/ifNeedWater/{plantId}")
     public ResponseEntity<ApiResponse<Boolean>> ifNeedWater(@PathVariable Long plantId) {
         return ResponseEntity.ok(ApiResponse.onSuccess(plantService.ifNeedWater(plantId)));
