d029ae18b381ecb5ce6192d95c23f69a918247c2
acidify04 <peh4622@naver.com>
2025-11-03 14:39:53 +0900

    hotfix: swagger, health check API에 로그인 없이 접근 가능하도록 수정

--- sjseed/sjseed/src/main/java/com/capstone/sjseed/config/SecurityConfig.java
+++ sjseed/sjseed/src/main/java/com/capstone/sjseed/config/SecurityConfig.java
@@ -33,7 +33,9 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/member/signup", "/api/member/signin").permitAll()
+                        .requestMatchers("/api/member/signup", "/api/member/signin", "health", "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
